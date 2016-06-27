31540ccc5f7803f61139be93faab48c4bed61fcc
tangdan16 <2398106931@qq.com>
2016-06-27 16:25:56 +0800

    new add new to drag.js

--- drag.js
+++ drag.js
@@ -5,8 +5,10 @@ function drag(id){
 	var disX = 0;
 	var disY = 0;
 
-	obj.onmousedown = function(){
+	obj.onmousedown = function(ev){
 		var num = 0;
+		disX = ev.clienetX - disX;
+		disY = ev.clienetY - disY;
 	}
 
 	console.log('第n次提交了');
