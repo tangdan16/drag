f37e8481bdacb5b558a5081ff959d2f82d7bd7a3
tangdan16 <2398106931@qq.com>
2016-06-28 14:41:44 +0800

    recover code

--- drag.js
+++ drag.js
@@ -1,5 +1,22 @@
 // JavaScript Document
 
 function drag(id){
-	//hahah我把他们都删除了
+	var obj = document.getElementById(id);
+	var disX = 0;
+	var disY = 0;
+
+	obj.onmousedown = function(ev){
+		var num = 0;
+		disX = ev.clienetX - disX;
+		disY = ev.clienetY - disY;
+
+		document.onmousemove = function(){
+			var num=22222222222;
+		}
+		console.log('已经很完善了！');
+	}
+
+	console.log('第n次提交了');
+	console.log('烦躁');
+	// 我又恢复到了之前的代码
 }
