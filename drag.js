b7d8566f5ed0d55ce92819bf99ceeb1da7d8f1d7
tangdan16 <2398106931@qq.com>
2016-06-28 14:39:16 +0800

    delete drag.js

--- drag.js
+++ drag.js
@@ -1,21 +1,5 @@
 // JavaScript Document
 
 function drag(id){
-	var obj = document.getElementById(id);
-	var disX = 0;
-	var disY = 0;
-
-	obj.onmousedown = function(ev){
-		var num = 0;
-		disX = ev.clienetX - disX;
-		disY = ev.clienetY - disY;
-
-		document.onmousemove = function(){
-			var num=22222222222;
-		}
-		console.log('已经很完善了！');
-	}
-
-	console.log('第n次提交了');
-	console.log('烦躁');
+	//hahah我把他们都删除了
 }
